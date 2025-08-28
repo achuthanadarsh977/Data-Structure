094571f9342c97da195b4629dd725b1eb0ada8ff
Adarsh Achuthan <160232793+achuthanadarsh977@users.noreply.github.com>
2025-08-28 17:48:31 +0530

    Create Trees.ts

---
+++ The Ultimate Data Structures & Algorithms: Part 2/C1 - Trees/Trees.ts
@@ -0,0 +1,51 @@
+class TreeNode {
+    value: number;
+    children: TreeNode[] = [];
+
+    constructor(value: number) {
+        this.value = value;
+    }
+}
+
+class Tree {
+    root: TreeNode | null = null;
+
+    constructor(value: number) {
+        this.root = new TreeNode(value);
+    }
+
+    addChild(parentValue: number, childValue: number): void {
+        const parent = this.find(this.root, parentValue);
+        if (parent) {
+            parent.children.push(new TreeNode(childValue));
+        }
+    }
+
+    private find(node: TreeNode | null, value: number): TreeNode | null {
+        if (!node) return null;
+        if (node.value === value) return node;
+
+        for (let child of node.children) {
+            let found = this.find(child, value);
+            if (found) return found;
+        }
+
+        return null;
+    }
+
+    traverse(node: TreeNode | null = this.root): void {
+        if (!node) return;
+        console.log(node.value);
+        for (let child of node.children) {
+            this.traverse(child);
+        }
+    }
+}
+
+
+const tree1 = new Tree(12);
+
+tree1.addChild(12,13);
+tree1.addChild(13,14);
+tree1.addChild(14,15);
+tree1.traverse()
